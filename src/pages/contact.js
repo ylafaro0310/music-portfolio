56b453af9e881d31fc6d4023d5287f83730b671a
ylafaro0310 <y16mukurou@gmail.com>
2019-12-04 02:46:48 +0900

    Modify contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
-const Contact = () => (
-    <div>
-        y16mukurou@gmail.com
-    </div>
-)
+class Contact extends React.Component {
+    render(){
+        return (
+            <div>
+                y16mukurou@gmail.com
+            </div>
+        )
+    }
+}
