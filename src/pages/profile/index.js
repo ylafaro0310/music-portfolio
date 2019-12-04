a68316ae53ec1ae578ca5d33cf3dd788146ac283
ylafaro0310 <y16mukurou@gmail.com>
2019-12-04 20:52:07 +0900

    Add profile image

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import Img from 'gatsby-image'
 import Layout from '../../components/Layout'
 import Cover from '../../components/Cover'
 
-const Profile = () => (
+const Profile = ({data}) => (
     <Layout>
         <Cover>
         <div>
             <h2>Profile</h2>
+            <Img fixed={data.file.childImageSharp.fixed} />
             <p>
             ウォーキングベースを好む1995年製ベーシスト。
             <br/>
@@ -27,3 +29,17 @@ const Profile = () => (
 )
 
 export default Profile
+
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "profile.jpg" }) {
+      childImageSharp {
+        # Specify the image processing specifications right in the query.
+        # Makes it trivial to update as your page's design changes.
+        fixed(width: 300) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
